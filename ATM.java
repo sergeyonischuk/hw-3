b210faab363406e96a8702cdcf31e665126adf8f
Sergey Onischuk <sergey.onischuk@ukr.net>
2022-04-15 11:47:35 +0300

    removed static from signature

--- ATM.java
+++ ATM.java
@@ -3,7 +3,7 @@ public class ATM {
 
     }
     
-    public static int countBanknotes(int sum) {
+    public int countBanknotes(int sum) {
         int[] banknotes = {500, 200, 100, 50, 20, 10, 5, 2, 1};
         int result = 0;
         int i = 0;
